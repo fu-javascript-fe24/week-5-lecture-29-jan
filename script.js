12290179ce38a137ff6823d529bf483685463efd
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-01-29 10:16:49 +0100

    created global array and function to populate it

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 console.log('Hello from script');
+
+let globalUsers = [];
+
+function initGlobalUsers() {
+    globalUsers.push({
+        username : 'Jesper',
+        password : 'fotboll'
+    });
+    globalUsers.push({
+        username : 'Theodr',
+        password : 'lego'
+    });
+    globalUsers.push({
+        username : 'Liam',
+        password : 'innebandy'
+    });
+    globalUsers.push({
+        username : 'Arne',
+        password : 'sågverk'
+    });
+}
