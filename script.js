a540a3690777b918d05d2900ad57dadf28ca880f
Jesper Nyberg <91jesnyb@gafe.molndal.se>
2025-01-29 10:36:01 +0100

    finished renderUsers

--- script.js
+++ script.js
@@ -2,4 +2,11 @@ console.log('Hello from script');
 
 function renderUsers() {
     const bodyRef = document.querySelector('body');
+
+    for(let user of globalUsers) {
+        let pRef = document.createElement('p');
+        pRef.textContent = user.username;
+        bodyRef.appendChild(pRef);
+        console.log(pRef);
+    }
 }
